e5a24368904f63f6ddbae63402a5602eec3bf6e0
huyhoang242001 <huyhoang.pham@secomus.com>
2023-12-09 13:28:47 +0700

    update search job

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/matching-job', [SearchController::class, 'searchJob']);
+Route::post('/search-job', [SearchController::class, 'searchJob']);
