8522f578a306ea70c7f6712bb57d151a1e2a1eed
trinhbang <huybang.trinh@secomus.com>
2023-12-29 20:37:09 +0700

    update middleware

--- routes/api.php
+++ routes/api.php
@@ -23,11 +23,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/search-job', [SearchController::class, 'searchJob']);
-Route::get('/list-business', [BusinessController::class, 'list']);
-Route::get('/job-detail/{id}', [JobPostingController::class, 'getDetail']);
-Route::get('/review-search', [ReviewController::class, 'searchReview']);
-Route::match(['post', 'put'], '/review-reaction', [UserReviewReactionController::class, 'reaction']);
-Route::post('/create-comment', [ReviewCommentController::class, 'create']);
-Route::get('/get-similar-job', [SearchController::class, 'getSimilarJob']);
-Route::get('/review-detail/{id}', [ReviewController::class, 'getReviewById']);
+
+Route::group(['middleware' => ['cors']], function () {
+    Route::post('/search-job', [SearchController::class, 'searchJob']);
+    Route::get('/list-business', [BusinessController::class, 'list']);
+    Route::get('/job-detail/{id}', [JobPostingController::class, 'getDetail']);
+    Route::get('/review-search', [ReviewController::class, 'searchReview']);
+    Route::match(['post', 'put'], '/review-reaction', [UserReviewReactionController::class, 'reaction']);
+    Route::post('/create-comment', [ReviewCommentController::class, 'create']);
+    Route::get('/get-similar-job', [SearchController::class, 'getSimilarJob']);
+    Route::get('/review-detail/{id}', [ReviewController::class, 'getReviewById']);
+});
